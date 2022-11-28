be250716619a2fb5cf4c1288d2299c16c2e36557
shrujandev <shrujandev1824@gmail.com>
2022-11-28 21:57:43 +0530

    Join command for no participation

--- Backend/joins.sql
+++ Backend/joins.sql
@@ -16,3 +16,8 @@ MEMBERS_415 as M join Volunteers_415 as V where V.Event_ID = "E22001" and M.Memb
 select E.event_name as Event_Name ,E.event_type as Event_Type ,E.event_date as Date ,M.Name as Manager_Name,
 O.Org_Name as Organization from Event_415 as E join MEMBERS_415 as M On E.Manager_ID = M.Member_ID Join 
 Organization_415 as O on E.Org_ID = O.Org_ID;
+
+
+-- taking part in nothing 
+select M.Member_ID as Member_ID,M.name as Name,M.gender as Gender from 
+MEMBERS_415 as M where not exists (select * from Volunteers_415 as V where V.Member_ID=M.Member_ID);
