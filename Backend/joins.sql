254bcb7832a0da9f521e8cf61b6a7258bc13965b
shrujandev <shrujandev1824@gmail.com>
2022-11-28 18:45:14 +0530

    Member join added

--- Backend/joins.sql
+++ Backend/joins.sql
@@ -6,4 +6,5 @@ MEMBERS_415 as M join Event_415 as E where m.Member_ID = E.Manager_ID;
 
 
 
-select 
+select M.Member_ID as Member_ID,M.name as Name,M.gender as Gender from 
+MEMBERS_415 as M join Volunteers_415 where Volunteers_415.Event_ID = "E22001" group by Member_ID;
