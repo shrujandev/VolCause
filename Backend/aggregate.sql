830b8cd787b842e911f793cc0389911e7a5ecfd8
shrujandev <shrujandev1824@gmail.com>
2022-11-29 11:49:37 +0530

    update aggregate function added volunteer count and manager count

--- Backend/aggregate.sql
+++ Backend/aggregate.sql
@@ -1,6 +1,16 @@
-DELIMITER $$
-CREATE PROCEDURE getAge()
-BEGIN
-UPDATE Members_415 SET Age= DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), dob)), '%Y') + 0;
-END;$$
-DELIMITER ;
+--No of volunteers in the event 
+select E.Event_Name as Event_Name, count(V.Member_ID) as Number_of_Volunteers
+from
+Event_415 as E join Volunteers_415 as V on E.Event_ID = V.Event_ID group by(V.Event_ID);
+
+
+--event per manager
+select M.Manager_Name as Manager_Name,count(M.Event_ID) as Events from Manager_415 as M group by (Manager_ID);
+
+
+--Event per volunteer 
+select M.Member_Id as Member_ID,M.name as Name,count(E.event_ID) as Number_of_Events 
+from Members_415 as M left join Volunteers_415 as E on m.Member_ID = E.Member_ID group by (M.Member_ID);
+
+
+--
