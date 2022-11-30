48f72f35823fb326c194502f20e5293853e1a0ce
shrujandev <shrujandev1824@gmail.com>
2022-11-30 18:42:16 +0530

    app.py updated

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,17 +1,18 @@
 import mysql.connector
 import streamlit as st
 from create import create
-from delete import update
 from read import read
 from delete import delete
 from update import update
+from database import create_table
+from custom import custom
 
 
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="",
-    datebase="volcause_415"
+    database="volcause_415"
 )
 
 c = mydb.cursor()
@@ -21,10 +22,10 @@ c.close()
 
 def main():
     st.title("VolCause")
-    menu = ["ADD", "View", "Edit", "Remove"]
+    menu = ["Register", "View", "Edit", "Remove", "Custom"]
     choice = st.sidebar.selectbox("Menu", menu)
     create_table()
-    if choice == "ADD":
+    if choice == "Register":
         st.subheader("Enter details:")
         create()
     elif choice == "View":
@@ -36,6 +37,9 @@ def main():
     elif choice == "Remove":
         st.subheader("Delete Member Details")
         delete()
+    elif choice == "Custom":
+        st.subheader("Custom SQL Query")
+        custom()
     else:
         st.subheader("About tasks")
 
